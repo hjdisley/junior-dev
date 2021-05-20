965cb5ba0c95b0bb9449522e2bb9915c2900814d
Hayden Disley <haydendisley@Haydens-MacBook-Pro.local>
2021-05-20 15:43:49 +0100

    scrape indeed added

--- backend/scrapers/scrapeIndeed.js
+++ backend/scrapers/scrapeIndeed.js
@@ -3,11 +3,19 @@ const puppeteer = require("puppeteer");
 const scrapeIndeed = async (url) => {
   const browser = await puppeteer.launch({ headless: true });
   const page = await browser.newPage();
-  page.goto(url);
-  let jobs = await page.evaluate(() => {
-    document.querySelectorAll(".no-wrap").innerText;
-  });
+  await page.goto(url);
+  const jobs = await page.evaluate(() =>
+    Array.from(document.querySelectorAll(".jobsearch-SerpJobCard")).map(
+      (el) => ({
+        title: el.querySelector(".title").innerText.trim(),
+        company: el.querySelector(".company").innerText,
+        location: el.querySelector(".location").innerText,
+        description: el.querySelector(".summary li").innerText,
+      })
+    )
+  );
   console.log(jobs);
+  await browser.close();
 };
 
 scrapeIndeed("https://uk.indeed.com/jobs?q=junior+developer&l=United+Kingdom");
